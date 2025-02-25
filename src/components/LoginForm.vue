d24eb4bddc9dfa586e5a2d6b6acd0313241de365
NabilMustofaa <nabilmustofa6@gmail.com>
2025-02-25 11:19:11 +0700

    feature login completed

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,35 @@
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+import store from '../store'
+import router from '../router'
+
+const email = ref('')
+const password = ref('')
+
+
+const handleLogin = async () => {
+  try {
+    store.dispatch('login', { email: email.value, password: password.value })
+    router.push({ name: 'Dashboard' })
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+
+
+</script>
+
+<template>
+ <div class="flex my-8 border">
+   <input placeholder="email" type="email" v-model="email"/>
+   <input placeholder="password" type="password" v-model="password"/>
+   <button @click="handleLogin" class="bg-blue-500">Login</button>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
